5e962bf91d38941699125f6140a41c546cce3eda
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-06-16 11:07:22 -0700

    language selector

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react'
 
 export default class App extends Component {
-  render() {
-    return (
-      <div>App</div>
-    )
-  }
+    state = { language: 'english' };
+
+    onLanguageChange = (language) => {
+        this.setState({ language });
+    };
+
+    render() {
+        return (
+            <div className='ui container'>
+                <div>
+                    Select a language:
+                    <i className='flag us' onClick={() => this.onLanguageChange('english')} />
+                    <i className='flag nl' onClick={() => this.onLanguageChange('dutch')} />
+                </div>
+                {this.state.language}
+            </div>
+        )
+    }
 }
