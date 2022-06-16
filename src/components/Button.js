2aa9f7c2d50f06b8b87b5c02684c0ba63892e4d9
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-06-16 11:21:20 -0700

    creating language context

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import LanguageContext from '../contexts/LanguageContext';
 
 export default class Button extends Component {
-  render() {
-    return (
-      <button className='ui button primary'>Submit</button>
-    )
-  }
+    static contextType = LanguageContext;
+
+
+    render() {
+        return (
+            <button className='ui button primary'>Submit</button>
+        )
+    }
 }
